e8a5d7ab59e81e1b6aee7f66a1a817c986adf93f
Rengevich Sergei <ser-rengevich@yandex.ru>
2020-05-28 14:49:15 +0300

    5 проектная работа(1 вариант)

--- script/script.js
+++ script/script.js
@@ -1,62 +1,185 @@
-/* Присваиваем переменные постоянных значений DOM-элементам.*/
-const editButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
-const form = document.querySelector(".popup__form");
+/*-----МАССИВ ПЕРЕМЕННЫХ-----*/
+const initialCards = [
+  {
+    name: 'Архыз',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
 
-const fullName = document.querySelector(".profile__full-name");
-const description = document.querySelector(".profile__description");
-const popupFullName = document.querySelector(".popup__input_full-name");
-const popupDescription = document.querySelector(".popup__input_description");
+const profileEditButton = document.querySelector('.profile__edit-button'); //кнопка редактирование профиля
+const popupEditProfile = document.querySelector('.popup'); // окно редактирование профиля
+const profileCloseButton = document.querySelector('.popup__close-button'); //крестик на попапе редактирования профиля
+const saveFormEditProfile = document.querySelector('.popup__form'); // вся форма попап
 
-/* Функция открытия модального окна. 
-(буду думать, как объединить их в одну функцию - пока не получается рабочий вариант!)*/
-function openPopup() {
-  popup.classList.add("popup_opened"); /* Добавляем класс DOM-элементу popup*/
+const fullName = document.querySelector('.profile__full-name'); //имя профиля
+const description = document.querySelector('.profile__description'); // подпись профиля
+const popupFullName = document.querySelector('.popup__input_full-name'); //значения имени в попап профиля
+const popupDescription = document.querySelector('.popup__input_description'); //значения подписи в попап профиля
 
-  /* Подставляем данные из переменной страницы в модальное окно*/
-  popupFullName.value = fullName.textContent;
-  popupDescription.value = description.textContent;
-}
+const addButton = document.querySelector('.profile__add-button'); // кнопка добавление карточек
+const popupAddForm = document.querySelector('.popup_add-form'); //попап добавления*/
+const popupCloseAddForm = document.querySelector(
+  '.popup__close-button_add-form'
+); /*кнопка закрытия попап карточек*/
+const elementCardField = document.querySelector('.elements'); //выбираем весь блок с карточками
+const elementTemplate = document.querySelector('.element-template').content;
 
-/* Функция закрытия модального окна*/
-function closePopup() {
-  /* Удаляем класс DOM-элемента popup*/
-  popup.classList.remove("popup_opened");
-}
+const popupTitle = document.querySelector('.popup__input_title'); // значение подписи в попап добавления карточки
+const popupUrl = document.querySelector('.popup__input_url'); // значение адреса в попап добавления карточки
+const popupAddCard = document.querySelector('.popup__form_add'); //
 
-/* Функция отправки и сохранения изменений на странице из модального окна*/
-function saveProfile(e) {
-  /*отмена отправки формы*/
-  e.preventDefault();
+const popupImage = document.querySelector('.popup-image'); // попаg-блок - галерея картинок
+const popupImageFrame = document.querySelector('.popup-image__frame'); // картинка в попап-блоке галереии картинок
+const popupImageTitle = document.querySelector('.popup-image__title'); // подпись в попап-блоке галереии картинок
+const popupImageCloseButton = document.querySelector(
+  '.popup-image__close-button'
+);
 
-  /*Подставляем данные из переменной модального окна в переменную страницы.*/
-  fullName.textContent = popupFullName.value;
-  description.textContent = popupDescription.value;
-
-  closePopup();
-}
-
-/* Присваиваем слушателей на события: 
-1)клик на крестик, 2) клик на кнопку редактирования, 3) отслеживание отправки формы*/
-closeButton.addEventListener("click", closePopup);
-editButton.addEventListener("click", openPopup);
-form.addEventListener("submit", saveProfile);
-
-/* Функционал по объединению функций открытия и закрытия в одну функцию
-function modal () {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.add("popup_opened");
+/*-----НИЖЕ ФУНКЦИОНАЛ РАБОТЫ С РЕДАКТИРОВАНИЕМ ПРОФИЛЯ-----*/
+/* Функция открытия и закрытия окна редактирования профиля
+ добавляет текст из модального окна на страницу*/
+function modalEditProfile() {
+  if (popupEditProfile.classList.toggle('popup_opened')) {
     popupFullName.value = fullName.textContent;
     popupDescription.value = description.textContent;
-    console.log('test')
-  }  
-  else {
-    popup.classList.remove("popup_opened");
   }
-} 
+}
 
-Добавить отслеживание нажатия кнопки и разобраться, почему не определяется если при закрытии 
+/* Функция закрытия попап-блок с галереей картинок*/
+function closePopupImage() {
+  popupImage.classList.remove('popup_opened');
+}
 
-Разобраться, почему не работает функция
-*/
+/* Функция изменения информации на странице профиля через модальное окно с получением информации, отображаемой на странице
+Получает информацию со страницы и вставляет в модальное окно*/
+function saveProfile(e) {
+  e.preventDefault();
+  fullName.textContent = popupFullName.value;
+  description.textContent = popupDescription.value;
+  modalEditProfile();
+}
+
+/*-----НИЖЕ ФУНКЦИОНАЛ РАБОТЫ С ДОБАВЛЕНИЕМ КАРТОЧЕК-----*/
+/* Функция открытия модального окна добавления элементов с картинками*/
+function modalAddForm() {
+  if (popupAddForm.classList.toggle('popup_opened')) {
+    popupUrl.value = '';
+    popupTitle.value = '';
+  }
+}
+
+/*функция добавления карточек на страницу из массива c правилом проверки для расположения*/
+function addElement(link, name, isInitial) {
+  const element = elementTemplate.cloneNode(true); //клонируем элемент
+  element.querySelector('.element__photo').src = link; //выбираем селектор и задаем  адрес картинки из массива
+  element.querySelector('.element__photo').alt = name; //задаем описание картинки исходя из её названия
+  element.querySelector('.element__text').textContent = name; // выбираем класс и задаем название картинки из массива
+
+  if (isInitial) {
+    elementCardField.append(element); //если элемент есть в массиве, то добавить последним
+  } else {
+    elementCardField.prepend(element); //в другом случае добавить в конце (проверка в параметре и в функции)
+  }
+}
+/*Выполнение перебора массива и выполнение для каждого элемента функции*/
+initialCards.forEach(function (element) {
+  addElement(element.link, element.name, true); //подставляем значения массива в функцию
+});
+
+/*функция добавления новой карточки из модального окна*/
+function addCard(event) {
+  event.preventDefault();
+  addElement(popupUrl.value, popupTitle.value, false);
+  modalAddForm();
+}
+
+/*функция удаления карточки,работы с лайком и открытия попап-блока галереии картинок*/
+function chooseCard(event) {
+  let deleteElement = event.target.closest('.element__basket');
+  let likeElement = event.target.closest('.element__button-like');
+  let elementPhoto = event.target.closest('.element__photo');
+
+  if (elementPhoto) {
+    popupImage.classList.toggle('popup_opened');
+    popupImageFrame.src = elementPhoto.src;
+    popupImageFrame.alt = elementPhoto.alt;
+    popupImageTitle.textContent = elementPhoto.alt;
+  }
+  if (deleteElement) {
+    deleteElement.parentNode.remove();
+  }
+  if (likeElement) {
+    likeElement.classList.toggle('element__button-like_on');
+  }
+}
+
+/*-----СЛУШАТЕЛИ СОБЫТИЙ-----*/
+profileEditButton.addEventListener('click', modalEditProfile); // слушатель кнопки редактирования профиля
+profileCloseButton.addEventListener('click', modalEditProfile); // слушатель крестика попапа редактирования профиля
+saveFormEditProfile.addEventListener('submit', saveProfile); // слушатель кнопки сохранить попап редактирования профиля
+addButton.addEventListener('click', modalAddForm); // слушатель кнопки добавления карточек
+popupCloseAddForm.addEventListener('click', modalAddForm); // слушатель крестика попапа добавления карточек
+popupAddCard.addEventListener('submit', addCard); //слушатель на кнопке сздать с функцией добавления карточки
+elementCardField.addEventListener('click', chooseCard); //слушатель на весь блок с карточками с выполнением функции
+popupImageCloseButton.addEventListener('click', closePopupImage); // слушатель на крестик в попап-блок галереии картинок
+
+/*-----СТАРЫЙ КОД (ПЕРЕРАБОТАТЬ/УЛУЧШИТЬ)-----*/
+
+// В ФУНКЦИИ CHOOSECARD ПЕРЕДЕЛАТЬ ПЕРЕМЕННЫЕ
+/* elementPhoto.src = link; (ПЕРЕДЕЛАТЬ В ПЕРСПЕКТИВЕ НА БОЛЕЕ ЧИТАЕМЫЙ И РАБОЧИЙ ВАРИАНТ (ЭТО ДЛЯ СЕБЯ))
+   elementText.textContent = name; */
+
+/*функция удаления карточек*/
+/*function deleteCard(event) {  
+  let deleteElement = event.target.closest('.element__basket');
+  if(deleteElement) {deleteElement.parentNode.remove();}  
+}
+/*функция добавления лайка*/
+/*function addLike (event) {
+  if (event.target.classList.contains('element__button-like')) {
+   event.target.closest('.element__button-like').classList.toggle('element__button-like_on');
+ }
+ }
+//elementField.addEventListener('click', addLike); //слушатель на весь блок с карточками с функцией добавления лайка.
+//elementField.addEventListener('click', deleteCard); // слушатель на весь блок с карточками с функцией удаления карточки
+
+//elementCardField.addEventListener('click', testing);
+
+/*function testing (event) {
+  let test = event.target.closest('.element');
+  console.log(event.target);    
+  console.log(test);
+  if (test) {
+    let imagetest = test.querySelector('.element__photo');
+    popupImage.classList.toggle('popup_opened');
+    popupImageFrame.src = imagetest.src;    
+    popupImageFrame.alt = imagetest.alt;
+    popupImageTitle.textContent = imagetest.alt;
+  }  
+}*/
