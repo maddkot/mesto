299a1af9a62091e50dc57019b734ab7b076c4f46
Rengevich Sergei <ser-rengevich@yandex.ru>
2020-05-31 01:44:48 +0300

    5 проектная работа(2 вариант)

--- script/script.js
+++ script/script.js
@@ -48,7 +48,7 @@ const popupCloseAddForm = document.querySelector(
   '.popup__close-button_add-form'
 ); /*кнопка закрытия попап карточек*/
 const elementCardField = document.querySelector('.elements'); //выбираем весь блок с карточками
-const elementTemplate = document.querySelector('.element-template').content;
+const elementTemplate = document.querySelector('.element-template').content; //выбор содержимого блока  карточки
 
 const popupTitle = document.querySelector('.popup__input_title'); // значение подписи в попап добавления карточки
 const popupUrl = document.querySelector('.popup__input_url'); // значение адреса в попап добавления карточки
@@ -61,125 +61,109 @@ const popupImageCloseButton = document.querySelector(
   '.popup-image__close-button'
 );
 
+/* Функция проверки наличия класса для открытия/закрытия модальных окон*/
+function togglePopup(popup) {
+  popup.classList.toggle('popup_opened');
+}
+
 /*-----НИЖЕ ФУНКЦИОНАЛ РАБОТЫ С РЕДАКТИРОВАНИЕМ ПРОФИЛЯ-----*/
 /* Функция открытия и закрытия окна редактирования профиля
  добавляет текст из модального окна на страницу*/
-function modalEditProfile() {
-  if (popupEditProfile.classList.toggle('popup_opened')) {
-    popupFullName.value = fullName.textContent;
-    popupDescription.value = description.textContent;
-  }
+function openProfilePopup() {
+  togglePopup(popupEditProfile);
+  popupFullName.value = fullName.textContent;
+  popupDescription.value = description.textContent;
 }
 
 /* Функция закрытия попап-блок с галереей картинок*/
 function closePopupImage() {
-  popupImage.classList.remove('popup_opened');
+  togglePopup(popupImage);
 }
 
 /* Функция изменения информации на странице профиля через модальное окно с получением информации, отображаемой на странице
 Получает информацию со страницы и вставляет в модальное окно*/
-function saveProfile(e) {
-  e.preventDefault();
+function saveProfile(event) {
+  event.preventDefault();
   fullName.textContent = popupFullName.value;
   description.textContent = popupDescription.value;
-  modalEditProfile();
+  togglePopup(popupEditProfile);
 }
 
-/*-----НИЖЕ ФУНКЦИОНАЛ РАБОТЫ С ДОБАВЛЕНИЕМ КАРТОЧЕК-----*/
+/*-----НИЖЕ ФУНКЦИОНАЛ РАБОТЫ С ДОБАВЛЕНИЕМ/УДАЛЕНИЕМ КАРТОЧЕК-----*/
 /* Функция открытия модального окна добавления элементов с картинками*/
 function modalAddForm() {
-  if (popupAddForm.classList.toggle('popup_opened')) {
-    popupUrl.value = '';
-    popupTitle.value = '';
-  }
+  togglePopup(popupAddForm);
+  popupUrl.value = '';
+  popupTitle.value = '';
 }
 
 /*функция добавления карточек на страницу из массива c правилом проверки для расположения*/
-function addElement(link, name, isInitial) {
-  const element = elementTemplate.cloneNode(true); //клонируем элемент
+function addElement(link, name) {
+  let element = elementTemplate.cloneNode(true); //клонируем элемент
   element.querySelector('.element__photo').src = link; //выбираем селектор и задаем  адрес картинки из массива
   element.querySelector('.element__photo').alt = name; //задаем описание картинки исходя из её названия
   element.querySelector('.element__text').textContent = name; // выбираем класс и задаем название картинки из массива
-
-  if (isInitial) {
-    elementCardField.append(element); //если элемент есть в массиве, то добавить последним
-  } else {
-    elementCardField.prepend(element); //в другом случае добавить в конце (проверка в параметре и в функции)
-  }
+  pasteCard(element); //вызываем функцию добавления карточки в разметку
 }
+
 /*Выполнение перебора массива и выполнение для каждого элемента функции*/
-initialCards.forEach(function (element) {
-  addElement(element.link, element.name, true); //подставляем значения массива в функцию
-});
+function firstLoadingCards() {
+  initialCards.reverse();
+  initialCards.forEach(function (element) {
+    addElement(element.link, element.name); //подставляем значения массива в функцию
+  });
+}
+
+/* вызываем функцию перебора массива с карточками для отображения*/
+firstLoadingCards();
+
+/* функция добавление карточки в разметку */
+function pasteCard(event) {
+  elementCardField.prepend(event);
+}
 
 /*функция добавления новой карточки из модального окна*/
 function addCard(event) {
   event.preventDefault();
-  addElement(popupUrl.value, popupTitle.value, false);
+  addElement(popupUrl.value, popupTitle.value);
   modalAddForm();
 }
 
-/*функция удаления карточки,работы с лайком и открытия попап-блока галереии картинок*/
-function chooseCard(event) {
+/* функция удаления карточки */
+function deleteClick(event) {
   let deleteElement = event.target.closest('.element__basket');
-  let likeElement = event.target.closest('.element__button-like');
-  let elementPhoto = event.target.closest('.element__photo');
-
-  if (elementPhoto) {
-    popupImage.classList.toggle('popup_opened');
-    popupImageFrame.src = elementPhoto.src;
-    popupImageFrame.alt = elementPhoto.alt;
-    popupImageTitle.textContent = elementPhoto.alt;
-  }
   if (deleteElement) {
-    deleteElement.parentNode.remove();
+    deleteElement.closest('.element').remove();
   }
+}
+
+/* функция удаления/добавления лайка*/
+function likeClick(event) {
+  let likeElement = event.target.closest('.element__button-like');
   if (likeElement) {
     likeElement.classList.toggle('element__button-like_on');
   }
 }
 
+/* функция открытия попап-блока с картинкой на полный экран*/
+function imageClick(event) {
+  let elementPhoto = event.target.closest('.element__photo');
+  if (elementPhoto) {
+    togglePopup(popupImage);
+    popupImageFrame.src = elementPhoto.src;
+    popupImageFrame.alt = elementPhoto.alt;
+    popupImageTitle.textContent = elementPhoto.alt;
+  }
+}
+
 /*-----СЛУШАТЕЛИ СОБЫТИЙ-----*/
-profileEditButton.addEventListener('click', modalEditProfile); // слушатель кнопки редактирования профиля
-profileCloseButton.addEventListener('click', modalEditProfile); // слушатель крестика попапа редактирования профиля
+profileEditButton.addEventListener('click', openProfilePopup); // слушатель кнопки редактирования профиля
+profileCloseButton.addEventListener('click', openProfilePopup); // слушатель крестика попапа редактирования профиля
 saveFormEditProfile.addEventListener('submit', saveProfile); // слушатель кнопки сохранить попап редактирования профиля
 addButton.addEventListener('click', modalAddForm); // слушатель кнопки добавления карточек
 popupCloseAddForm.addEventListener('click', modalAddForm); // слушатель крестика попапа добавления карточек
-popupAddCard.addEventListener('submit', addCard); //слушатель на кнопке сздать с функцией добавления карточки
-elementCardField.addEventListener('click', chooseCard); //слушатель на весь блок с карточками с выполнением функции
+popupAddCard.addEventListener('submit', addCard); //слушатель на кнопке сoздать с функцией добавления карточки
+elementCardField.addEventListener('click', deleteClick); // слушатель на блоке карточек с функцией удаления карточки
+elementCardField.addEventListener('click', likeClick); //слушатель на блоке карточекс функцией установки/снятия лайка
+elementCardField.addEventListener('click', imageClick); // слушатель на блоке карточек с функцией клика по картинке и открытия ее на весь экран
 popupImageCloseButton.addEventListener('click', closePopupImage); // слушатель на крестик в попап-блок галереии картинок
-
-/*-----СТАРЫЙ КОД (ПЕРЕРАБОТАТЬ/УЛУЧШИТЬ)-----*/
-
-// В ФУНКЦИИ CHOOSECARD ПЕРЕДЕЛАТЬ ПЕРЕМЕННЫЕ
-/* elementPhoto.src = link; (ПЕРЕДЕЛАТЬ В ПЕРСПЕКТИВЕ НА БОЛЕЕ ЧИТАЕМЫЙ И РАБОЧИЙ ВАРИАНТ (ЭТО ДЛЯ СЕБЯ))
-   elementText.textContent = name; */
-
-/*функция удаления карточек*/
-/*function deleteCard(event) {  
-  let deleteElement = event.target.closest('.element__basket');
-  if(deleteElement) {deleteElement.parentNode.remove();}  
-}
-/*функция добавления лайка*/
-/*function addLike (event) {
-  if (event.target.classList.contains('element__button-like')) {
-   event.target.closest('.element__button-like').classList.toggle('element__button-like_on');
- }
- }
-//elementField.addEventListener('click', addLike); //слушатель на весь блок с карточками с функцией добавления лайка.
-//elementField.addEventListener('click', deleteCard); // слушатель на весь блок с карточками с функцией удаления карточки
-
-//elementCardField.addEventListener('click', testing);
-
-/*function testing (event) {
-  let test = event.target.closest('.element');
-  console.log(event.target);    
-  console.log(test);
-  if (test) {
-    let imagetest = test.querySelector('.element__photo');
-    popupImage.classList.toggle('popup_opened');
-    popupImageFrame.src = imagetest.src;    
-    popupImageFrame.alt = imagetest.alt;
-    popupImageTitle.textContent = imagetest.alt;
-  }  
-}*/
